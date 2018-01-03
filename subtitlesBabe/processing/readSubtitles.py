07e20b0fa72d5140a0d9c0b258e8219d475cc240
Serafín <serapoint@users.noreply.github.com>
2018-01-03 21:33:23 +0100

    2 classes in readSubtitles - SRTSubtitle and SUBSubtitle buth with toText function

--- subtitlesBabe/processing/readSubtitles.py
+++ subtitlesBabe/processing/readSubtitles.py
@@ -1,4 +1,7 @@
 import pysrt
+import os
+import sys
+import re
 
 class SRTSubtitle:
     """Class encapsulation for SRT subtitle."""
@@ -7,7 +10,40 @@ class SRTSubtitle:
         """Instantiate
         Arguments:
             path (str) - Path to str file
-            encoding_ (str: 'utf-8') - encoding to use when oepning the file.
+            encoding_ (str: None) - encoding to use when oepning the file.
+        """
+
+        self.name = path.split("/")[-1][:-4]
+
+        # Set encoding to 'utf-8' unless specified in the instantiation
+        if bool(encoding):
+            self.encoding = encoding
+        else:
+            self.encoding = 'utf-8'
+
+        # Open subtitles file
+        try:
+            self.subtitle = pysrt.open(path,encoding = self.encoding)
+            print("Subtitles file successfully loaded with encoding \'" + self.encoding + "\'.")
+        except UnicodeDecodeError as e:
+            print(e)
+            print("Error interpreting the subtitles as "+ self.encoding+". Are these subtitles in English? Try selecting a different encoding using the 'encoding' argument.")
+
+        assert self.subtitle, "Subtitle loaded but it appears to be empty. Wrong encoding selected (maybe?)"
+
+
+    def toText(self):
+        """Return string containing all the captions from the file"""
+        return self.subtitle.text
+
+class SUBSubtitle:
+    """Class encapsulation for SUB subtitle."""
+
+    def __init__(self,path,encoding=None):
+        """Instantiate
+        Arguments:
+            path (str) - Path to str file
+            encoding_ (str: None) - encoding to use when oepning the file.
         """
 
         self.name = path.split("/")[-1][:-4]
@@ -19,14 +55,19 @@ class SRTSubtitle:
 
         # Open subtitles file
         try:
-            self.subtitle = pysrt.open(path)
+            self.subtitle = open(path).read()
             print("Subtitles file successfully loaded with encoding \'" + self.encoding + "\'.")
-        except UnicodeDecodeError:
+        except UnicodeDecodeError as e:
+            print(e)
             print("Error interpreting the subtitles as "+ self.encoding+". Are these subtitles in English? Try selecting a different encoding using the 'encoding' argument.")
-
-        assert self.subtitle, "Subtitle loaded but it appears to be empty. Wrong encoding selected (maybe?)"
 
 
     def toText(self):
         """Return string containing all the captions from the file"""
-        self.subtitle.text
+        text = ""
+        for x in self.subtitle.split("\n"):
+            match = re.findall(r"{\d*}{\d*}(.*)",x)[0]
+            text +=match
+            text +="\n"
+
+        return text
